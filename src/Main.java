c2f942f85756a9900aef5ce657aa9b23cb8af92c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-16 18:36:30 -0300

    objeto cachorro criado

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import Animais.Cachorro;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -6,5 +8,14 @@ public class Main {
             // short -32768 a 32767
             // char 0 a 65535
         System.out.println(variavelByte);
+
+        Cachorro cachorro1 = new Cachorro();
+        cachorro1.altura = 2;
+        cachorro1.cor = "Marrom";
+        cachorro1.nome = "jack";
+        cachorro1.peso = 2.52;
+        cachorro1.tamanhoDoRabo = 5;
+
+        System.out.println(cachorro1);
         }
     }
