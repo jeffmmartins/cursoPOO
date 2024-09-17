f0dd2066b4b061d0a2b3be144f000d96c14f180e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-17 18:44:16 -0300

    metodo set

--- src/Animais/Cachorro.java
+++ src/Animais/Cachorro.java
@@ -12,6 +12,46 @@ public class Cachorro {
       return this.nome;
    }
 
+   public String getCor() {
+      return cor;
+   }
+
+   public void setCor(String cor) {
+      this.cor = cor;
+   }
+
+   public int getAltura() {
+      return altura;
+   }
+
+   public void setAltura(int altura) {
+      this.altura = altura;
+   }
+
+   public double getPeso() {
+      return peso;
+   }
+
+   public void setPeso(double peso) {
+      this.peso = peso;
+   }
+
+   public int getTamanhoDoRabo() {
+      return tamanhoDoRabo;
+   }
+
+   public void setTamanhoDoRabo(int tamanhoDoRabo) {
+      this.tamanhoDoRabo = tamanhoDoRabo;
+   }
+
+   public String getEstadoDeEspirito() {
+      return estadoDeEspirito;
+   }
+
+   public void setEstadoDeEspirito(String estadoDeEspirito) {
+      this.estadoDeEspirito = estadoDeEspirito;
+   }
+
    public void comer(){}
 
    public void latir(){
