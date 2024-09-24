521ec0d5147843a47d6bb8bc34f7e796a2b7e933
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-24 18:55:41 -0300

    class animal

---
+++ src/Animais/animal.java
@@ -0,0 +1,15 @@
+package Animais;
+
+public class animal {
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private String estadoDeEspirito;
+
+    public void dormir(){}
+    public void comer(){}
+    public void soar(){
+        System.out.println("Piu Piu");
+    }
+}
