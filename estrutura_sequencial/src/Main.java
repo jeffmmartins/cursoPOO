c0357635e1a368e575a78b2387d67d70c4bd2db8
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-10-01 18:41:22 -0300

    importando o locale

--- estrutura_sequencial/src/Main.java
+++ estrutura_sequencial/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,5 +9,7 @@ public class Main {
         System.out.println(x);
         System.out.printf("%.2f%n", y);
         System.out.println("Boa noite");
+        Locale.setDefault(Locale.US);
+        System.out.printf("%.4f%n", y);
         }
 }
