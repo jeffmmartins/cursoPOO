0e0bb3f6968f00eb5326ff5429c7a5780ca75445
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-12-07 17:35:04 -0300

    exercicio criando uma classe produto

---
+++ course/src/Main.java
@@ -0,0 +1,26 @@
+import entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        Product product= new Product();
+        System.out.println("Enter product data");
+        System.out.println("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+
+
+        sc.close();
+    }
+}
