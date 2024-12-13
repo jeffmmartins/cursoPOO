ceef3f10c719b774694521a0c4e74a0a2b8a225b
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-12-13 06:22:50 -0300

    exemplo de laço for each

---
+++ lacoforeach/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String [] vect = new String[] {"jeff", "mae", "pai"};
+
+        for (String nome: vect){
+            System.out.println(nome);
+        }
+    }
+}
