49372d2fe158bdd6c59232bc8acc5a24f4fd9b60
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-06 11:50:54 -0300

    exemplo de enum finalizado

--- Enum/src/Main.java
+++ Enum/src/Main.java
@@ -7,7 +7,14 @@ import java.util.Date;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Order order = new Order(1080, new Date(),);
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+        System.out.println(order);
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println(os1);
+        System.out.println(os2);
+
 
     }
 }
