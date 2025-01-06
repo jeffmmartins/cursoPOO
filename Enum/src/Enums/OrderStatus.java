6fb4be3ee17f904054c849105cb69c2f2cb3efaa
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-06 09:38:20 -0300

    Iniciando exemplo de enum

---
+++ Enum/src/Enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package Enums;
+
+public enum OrderStatus {
+    PENDING_PAYMENT,
+    PROCESSING,
+    SHIPED,
+    DELIVERED,
+}
---
+++ Enum/src/Main.java
@@ -0,0 +1,13 @@
+import Entities.Order;
+import Enums.OrderStatus;
+
+import java.util.Date;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Order order = new Order(1080, new Date(),);
+
+    }
+}
