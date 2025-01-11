27baded96b5a23c98439fb1623da2e2f36a393c9
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-11 09:10:29 -0300

    objeto comment e post instanciados

--- exercicioPost/src/Main.java
+++ exercicioPost/src/Main.java
@@ -1,7 +1,17 @@
+import Entities.Comment;
+import Entities.Post;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        // instanciando o objeto comment
+        Comment c1 = new Comment("Have a nice trip");
+        Comment c2 = new Comment("Wow that's awesome");
+
+        //instanciando o objeto post
+        Post p1 = new Post()
 
     }
 }
