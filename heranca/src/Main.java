f386e1ccfdfa727e4f41fb5f67b69e68a2b7ec58
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-15 09:16:05 -0300

    exemplo de upcacasting e downcasting

--- heranca/src/Main.java
+++ heranca/src/Main.java
@@ -1,7 +1,37 @@
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingAccount;
+
+import javax.management.remote.JMXAddressable;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Account acc = new Account(1001, "Jeff", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Vera", 0.0 , 500.00);
+
+        // upcasting: objeto businessAccoun e atribuilo na variavel tipo account
+
+        Account acc1 = bacc;
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SavingAccount(1004, "ANa", 0.0, 0.01);
+
+        //Downcassting forcar a conversão de superclasse para subclasse, nem sempre da certo, tem que analisar com cuidado.
+
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(100.0);
+
+        if (acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("loan!");
+        }
+        if (acc3 instanceof SavingAccount){
+            SavingAccount acc5 = (SavingAccount) acc3;
+            acc5.updateBalnace();
+            System.out.println("Update!");
+        }
     }
 }
