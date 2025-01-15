befc1749e262365e8bb12cfd7bb0660a80513b44
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-15 08:40:39 -0300

    class savingaccount finalizada

--- heranca/src/entities/SavingAccount.java
+++ heranca/src/entities/SavingAccount.java
@@ -1,4 +1,28 @@
 package entities;
 
-public class SavingAccount extends BusinessAccount{
+public class SavingAccount extends Account{
+    private Double interestRate;
+
+    public SavingAccount(){
+        super();
+    }
+
+    public SavingAccount(Integer number, String holder, Double balance, Double interestRate) {
+        super(number, holder, balance);
+        this.interestRate = interestRate;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(Double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    // metodo para atualizar o saldo
+    public void updateBalnace(){
+        balance += balance * interestRate;
+    }
+
 }
