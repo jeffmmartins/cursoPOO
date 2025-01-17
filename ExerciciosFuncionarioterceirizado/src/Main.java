0b33b25bb579bb7e0b1745647cfd20a354a8ae46
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-17 12:32:08 -0300

    logica do for feita

--- ExerciciosFuncionarioterceirizado/src/Main.java
+++ ExerciciosFuncionarioterceirizado/src/Main.java
@@ -1,8 +1,45 @@
+import Entities.Employee;
+import jdk.jshell.execution.LoaderDelegate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        // Declarando a lista do tipo employee
+        List<Employee> list = new ArrayList<>();
+
+        System.out.println("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        // for para percorrer a lista
+        for (int  i = 0 ; i<=n; i++){
+            System.out.println("Employee  #" + i + "data: ");
+            System.out.println("Outsourced (y/n) ? ");
+            char ch = sc.next().charAt(1);
+            System.out.println("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.println("Hours: ");
+            int hours = sc.nextInt();
+            System.out.println("Value per hours: ");
+            double valuePerHour = sc.nextDouble();
+            if (ch == 'y'){
+                System.out.println("Additional Charge: ");
+                double additionalCharge = sc.nextDouble();
+            }
+        }
+
+
+        sc.close();
     }
 }
