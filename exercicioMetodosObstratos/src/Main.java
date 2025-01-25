f5576f6c493ed4cd74ffbeaf1c36878dcdc87230
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-25 19:22:19 -0300

    lógica do for inicializada

--- exercicioMetodosObstratos/src/Main.java
+++ exercicioMetodosObstratos/src/Main.java
@@ -1,9 +1,34 @@
+import Entiites.enums.Color;
+
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of shape: ");
+        int n = sc.nextInt();
+        for(int i = 0; i<n; i++) {
+            System.out.println("Shape #" + i +  "Data");
+            System.out.println("Rectangle or Circle (r/c) ?  ");
+            char letter = sc.next().charAt(0);
+            System.out.println("Color (Back/Blue/Red: ");
+            Color color = Color.valueOf(sc.next());
+
+            if ('r' == letter) {
+                System.out.println("Width: ");
+                Double widht = sc.nextDouble();
+                System.out.println("Heigth: ");
+                Double Height = sc.nextDouble();
+            }
+        }
+
+        sc.close();
 
     }
 }
