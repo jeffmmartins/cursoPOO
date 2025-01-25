251733563aba794cb76be80832276c876ce3ed9f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-25 18:33:14 -0300

    class abstract Shape finalizado

--- exercicioMetodosObstratos/src/Entities/Shape.java
+++ exercicioMetodosObstratos/src/Entities/Shape.java
@@ -1,4 +1,24 @@
 package Entities;
 
-public class Shape {
+import Entiites.enums.Color;
+
+public  abstract class Shape {
+    private Color color;
+
+    public Shape(){
+    }
+
+    public Shape(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public abstract double area();
 }
