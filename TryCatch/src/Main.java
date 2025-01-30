e66fb51a1061fc6cf8a416c4129a5c33b2fad76f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-01-29 21:17:32 -0300

    exemplo try catch

---
+++ TryCatch/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // vetor de string
+        try {
+            String[] vect = sc.nextLine().split(" ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid Position");
+        } catch (InputMismatchException e){
+            System.out.println("Input error");
+        }
+
+        System.out.println("End Program");
+
+
+        sc.close();
+    }
+}
