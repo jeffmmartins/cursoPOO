d1f16f966b2ae7c8aa4acaf84ccdcf1bf0cbdc29
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-02-03 09:36:09 -0300

    classe taxpayer criada

---
+++ execicioFixaocaoImposto/src/Entities/TaxPayer.java
@@ -0,0 +1,29 @@
+package Entities;
+
+public class TaxPayer {
+    private String name;
+    private Double anualIncome;
+
+    public  TaxPayer(){}
+
+    public TaxPayer(String name, Double anualIncome) {
+        this.name = name;
+        this.anualIncome = anualIncome;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAnualIncome() {
+        return anualIncome;
+    }
+
+    public Double tax(){
+        return;
+    }
+}
