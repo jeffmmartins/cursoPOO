6d1cc03dce608657c55607adcdb416faa169cc5b
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-02-05 09:52:58 -0300

    iniciando a lógica do for

--- execicioFixaocaoImposto/src/Main.java
+++ execicioFixaocaoImposto/src/Main.java
@@ -1,7 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of tax payers: ");
+        int numberTax = sc.nextInt();
+
+        for (int i = 0 ; numberTax < i; i++){
+            System.out.println("Tax payer #" + (i+1)  + " data: ");
+            System.out.println("Indiviidual or Company (i/c)? ");
+            char letter = sc.next().charAt(0);
+            sc.nextLine();
+            System.out.println("Name");
+            String  name = sc.nextLine();
+        }
+
+
+        sc.close();
     }
 }
