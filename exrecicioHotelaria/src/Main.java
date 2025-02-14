f171f5d702cf18b1503231c182c0e0cddb98ab2d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-02-14 20:13:21 -0300

    logica do if apagada

--- exrecicioHotelaria/src/Main.java
+++ exrecicioHotelaria/src/Main.java
@@ -24,35 +24,31 @@ public class Main {
         System.out.println("Check out date (dd/MM/yyyy): ");
         Date chekOut = sdf.parse(sc.next());
 
-        //metodo muito ruim
-        if (!chekOut.after(checkin)) {
-            System.out.println("Error in reservation ");
-        }else {
-            Reservation reservation = new Reservation(number,checkin,chekOut);
-            System.out.println("Reservation: " + reservation);
+        Reservation reservation = new Reservation(number,checkin,chekOut);
+        System.out.println("Reservation: " + reservation);
 
-            System.out.println();
-            System.out.println("Enter data to update the reservation: ");
-            System.out.println("Checkin date (dd/MM/yyyy): ");
-            checkin = sdf.parse(sc.next());
-            sc.nextLine();
-            System.out.println("Check out date (dd/MM/yyyy): ");
-            chekOut = sdf.parse(sc.next());
+        System.out.println();
+        System.out.println("Enter data to update the reservation: ");
+        System.out.println("Checkin date (dd/MM/yyyy): ");
+        checkin = sdf.parse(sc.next());
+        sc.nextLine();
+        System.out.println("Check out date (dd/MM/yyyy): ");
+        chekOut = sdf.parse(sc.next());
 
 
 
 
-               String error =  reservation.updatDates(checkin,chekOut);
-               if(error != null) {
-                   System.out.println("Error in Reservation " + error);
-               }else {
-                System.out.println("Reservation: " + reservation);
-            }
-
-            // metodo responsavel para atualizar as datas
-            reservation.updatDates(checkin, chekOut);
+           String error =  reservation.updatDates(checkin,chekOut);
+           if(error != null) {
+               System.out.println("Error in Reservation " + error);
+           }else {
             System.out.println("Reservation: " + reservation);
         }
+
+        // metodo responsavel para atualizar as datas
+        reservation.updatDates(checkin, chekOut);
+        System.out.println("Reservation: " + reservation);
+
 
         sc.close();
     }
