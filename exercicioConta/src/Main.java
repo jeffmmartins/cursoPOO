40cf356e60768aa9d13820d00369e397549bd37d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-02-22 18:34:58 -0300

    preparando o main"

--- exercicioConta/src/Main.java
+++ exercicioConta/src/Main.java
@@ -1,9 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Enter account Data: ");
+        System.out.println("Number: ");
+        int number = sc.nextInt();
+        System.out.println("Holder: ");
+        String name =  sc.nextLine();
+        sc.nextLine();
+        System.out.println("Initial balance: ");
+        double balance = sc.nextDouble();
+        System.out.println("WithDraw limit: ");
+        double limit = sc.nextDouble();
+
+
+        sc.close();
     }
+
 }
