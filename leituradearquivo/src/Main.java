4eb14246c3d799435e02f0c88d8b9b288315f54d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-02-27 19:00:59 -0300

    corrigindo bug

--- leituradearquivo/src/Main.java
+++ leituradearquivo/src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {//TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 
-        File file = new File("C:\\Users\\Jefferson\\Documents\\CursoJava");
+        File file = new File("C:\\Users\\Jefferson\\Documents\\CursoJava\\new.txt");
         Scanner sc = null;
         try {
             sc = new Scanner(file); // ao instanciar estou passando para ele o arquivo.
