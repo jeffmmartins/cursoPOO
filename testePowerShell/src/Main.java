7a38980b886d95126cd4668c446ac9942d44f45f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-03-15 19:50:18 -0300

    exemplo de use Locale

--- testePowerShell/src/Main.java
+++ testePowerShell/src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testando o power shell");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Digite a idade");
+        int idade = sc.nextInt();
+
+        System.out.println("Testando o power shell" + idade);
+
+        sc.close();
     }
 }
