38bda640940776bebb27f167039a22dc6e7f2557
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-03 16:25:57 -0300

    exemplo de leitura de arquivo

--- lendo_arquivo/src/Main.java
+++ lendo_arquivo/src/Main.java
@@ -1,15 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        //Instanciando um objeto file do tipo File;
+        File file = new File("C:\\Users\\Jefferson\\Documents\\testejava.txt");
+        Scanner sc = null;
+        try {
+            // instanciando o sc passando como argumento o file.
+            sc = new Scanner(file);
+            // verifica se tem uma nova linha no arquivo
+            while (sc.hasNextLine()){
+                // caso exista uma nova linha no arquivo é impresso o que tem nessa linha.
+                System.out.println(sc.nextLine());
+            }
+            //trantando o erro
+        } catch (IOException e) {
+            System.out.println("error: " + e.getMessage());
+        } finally {
+            // testando se o sc diferente de nulo
+            if (sc != null){
+                sc.close();
+            }
         }
     }
 }
