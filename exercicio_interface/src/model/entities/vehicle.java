ac6af7592c5fd1825e728277f31f7f22b1fa45f5
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-11 09:33:13 -0300

    feat: classe vehicle criada

---
+++ exercicio_interface/src/model/entities/vehicle.java
@@ -0,0 +1,17 @@
+package model.entities;
+
+public class vehicle {
+    private String model;
+
+    public vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
