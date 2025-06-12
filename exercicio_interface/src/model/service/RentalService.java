db79f15a24f35833efb731ab692d56015dcfc7c6
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-12 17:18:34 -0300

    feat: lógica do processInvoice criada

--- exercicio_interface/src/model/service/RentalService.java
+++ exercicio_interface/src/model/service/RentalService.java
@@ -3,6 +3,9 @@ package model.service;
 import model.entities.CarRental;
 import model.entities.Invoice;
 
+import javax.net.ssl.HostnameVerifier;
+import java.time.Duration;
+
 public class RentalService {
 
     private Double pricePerHour;
@@ -17,6 +20,18 @@ public class RentalService {
     }
 
     public void processInvoice(CarRental carRental){
-        carRental.setInvoice(new Invoice(50.0, 10.0));
+
+        Double minutes = (double) Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60;
+
+        double basicpaymente;
+        if (hours <= 12){
+            basicpaymente = pricePerHour * Math.ceil(hours);
+        }else {
+            basicpaymente = pricePerDay * Math.ceil(hours/24.0);
+        }
+
+        double tax = brazilTaxService.tax(basicpaymente);
+        carRental.setInvoice(new Invoice(basicpaymente, tax));
     }
 }
--- processo_seletivo/src/candidatura/ProcessoSeletivo.java
+++ processo_seletivo/src/candidatura/ProcessoSeletivo.java
@@ -1,10 +1,36 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
 
     }
 
+    static void selecaoDeCandidatos(){
+        String [] candidatos = {"Ana Clara",
+                "João Pedro",
+                "Mariana Silva",
+                "Lucas Oliveira",
+                "Beatriz Santos",
+                "Gabriel Costa",
+                "Laura Fernandes",
+                "Pedro Almeida",
+                "Sofia Rodrigues",
+                "Rafael Pereira"
+        };
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while (candidatosSelecionados < 5){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
 
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0;
