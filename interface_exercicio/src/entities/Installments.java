d1d31946f607bd6b80ce51628a33733980a850ec
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-06-23 15:15:23 -0300

    Feat:exercicios de interface curso nelio alves, classe Installments criada

---
+++ interface_exercicio/src/entities/Installments.java
@@ -0,0 +1,32 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class Installments {
+
+    private LocalDate date;
+    private Double amount;
+
+    public Installments(){}
+
+    public Installments(Double amount, LocalDate date) {
+        this.amount = amount;
+        this.date = date;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
