6a8f63860e865147ac3982642c7358e6ddb5e9e6
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-01 18:00:32 -0300

    Feat: classe PrintService criada

--- generics/src/README.md
+++ generics/src/README.md
@@ -1,2 +1,3 @@
 Anotações Generics
 Permite que classes, interfaces e metodos possam ser parametrizados por tipo.
+Size() -> realiza a contagem a partir de 1;
---
+++ generics/src/service/PrintService.java
@@ -0,0 +1,33 @@
+package service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService {
+
+    private List<Integer> list = new ArrayList<>();
+
+    public void addValue(Integer value){
+        list.add(value);
+    }
+
+    public Integer first(){
+        if(list.isEmpty()){
+            throw new IllegalStateException("List is empty");
+        }
+        // metodo get() padrão java;
+        return list.get(0);
+    }
+
+    public void print(){
+        System.out.println(" [");
+        if (!list.isEmpty()){
+            System.out.println(list.get(0));
+        }
+        for (int i = 1; i < list.size(); i++){
+            System.out.println(", " + list.get(i));
+        }
+        System.out.println(" ]");
+    }
+
+}
