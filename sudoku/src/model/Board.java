61d1bc43ceabddea7fa35f25f6d31da2e40e7b1c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-19 19:08:44 -0300

    Feat: Classe Board criada

--- sudoku/src/model/Board.java
+++ sudoku/src/model/Board.java
@@ -1,6 +1,9 @@
 package model;
 
+import java.util.Collection;
 import java.util.List;
+
+import static java.util.Objects.*;
 
 public class Board {
 
@@ -13,4 +16,46 @@ public class Board {
     public List<List<Space>> getSpaces() {
         return spaces;
     }
+
+    public GameStatusEnum getStatus(){
+        if (spaces.stream().flatMap(Collection::stream).noneMatch(s -> !s.isFixed() && nonNull(s.getActual()))){
+            return GameStatusEnum.NON_STARTED;
+        }
+        return spaces.stream().flatMap(Collection::stream).anyMatch(s -> isNull(s.getActual())) ? GameStatusEnum.INCOMPLETE : GameStatusEnum.COMPLETE;
+    }
+
+    public boolean hasError(){
+        if (getStatus() == GameStatusEnum.NON_STARTED){
+            return false;
+        }
+        return spaces.stream().flatMap(Collection::stream).anyMatch(s -> nonNull(s.getActual()) && !s.getActual().equals((s.getExpected())));
+    }
+
+    public boolean changeValue(final int col, final int row, final Integer value){
+        var space = spaces.get(col).get(row);
+        if(space.isFixed()){
+            return false;
+        }
+        space.setActual(value);
+        return true;
+    }
+
+    public boolean clearValue(final int col, final int row){
+        var space = spaces.get(col).get(row);
+        if(space.isFixed()){
+            return false;
+        }
+
+        space.clearSpace();
+        return true;
+    }
+
+    public void reset(){
+        spaces.forEach(c -> c.forEach(Space::clearSpace));
+    }
+
+    public boolean gameIsfinish(){
+        return !hasError() && getStatus() == GameStatusEnum.COMPLETE;
+    }
+
 }
