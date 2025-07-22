25d0a0784a004d95ad18bdc5b9f80d907346e5a0
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-22 12:18:19 -0300

    Feat: inicializando a classe main

--- sudoku/src/Main.java
+++ sudoku/src/Main.java
@@ -1,15 +1,45 @@
+import model.Board;
+
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toMap;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    final static static Scanner scanner = new Scanner(System.in);
+
+    private static Board board;
+
+    private final static int BOARD_LIMIT = 9;
+    public static void main(String[] args) {
+        final var positions = Stream.of(args)
+                .collect(toMap(
+                        k -> k.split(";")[0],
+                        v -> v.split(";")[1]
+                ));
+
+       var option = -1;
+       while(true){
+           System.out.println("Selecione uam das opeções a seguir");
+           System.out.println("1 - Iniciar um novo jogo");
+           System.out.println("2 - COlocar um novo número");
+           System.out.println("3 - Remover um número");
+           System.out.println("4 - Visualizar jogo atual");
+           System.out.println("5 - verificar status do jogo");
+           System.out.println("6 - limpar jogo");
+           System.out.println("7 - Finalizar jogo");
+           System.out.println("8 - Sair");
+
+           option = scanner.nextInt();
+       }
+
+
+
+
+
     }
 }
