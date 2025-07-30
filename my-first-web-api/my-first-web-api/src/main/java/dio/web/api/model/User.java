fa81e1c8afe5ef5fbbad14549041c92ed3a92a64
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-07-30 15:52:37 -0300

    Feat: classe User criada

---
+++ my-first-web-api/my-first-web-api/src/main/java/dio/web/api/model/User.java
@@ -0,0 +1,45 @@
+package dio.web.api.model;
+
+public class User {
+
+    private Long id;
+    private String login;
+    private String password;
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "nome='" + login + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
