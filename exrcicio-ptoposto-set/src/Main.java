88f595f0875b737948394df8d8c0129d018d2e4c
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-08-25 16:22:30 -0300

    Refactor: alterando a lógica

--- exrcicio-ptoposto-set/src/Main.java
+++ exrcicio-ptoposto-set/src/Main.java
@@ -7,32 +7,43 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        Set<IdStudents> set = new HashSet<>();
-
-        System.out.println("how many students couse A");
-        set.add(new IdStudents(sc.nextInt()));
-        System.out.println("how many students couse A");
-        set.add(new IdStudents(sc.nextInt()));
-        System.out.println("how many students couse A");
-        set.add(new IdStudents(sc.nextInt()));
-        System.out.println("how many students couse A");
-        set.add(new IdStudents(sc.nextInt()));
-        System.out.println("how many students couse A");
-        set.add(new IdStudents(sc.nextInt()));
-        System.out.println("how many students couse A");
-        set.add(new IdStudents(sc.nextInt()));
-        System.out.println("how many students couse A");
-        set.add(new IdStudents(sc.nextInt()));
-        System.out.println("how many students couse A");
-        set.add(new IdStudents(sc.nextInt()));
-
-        System.out.println("Total students: " + set.size());
 
 
+            Scanner sc = new Scanner(System.in);
+
+            Set<Integer> a = new HashSet<>();
+            Set<Integer> b = new HashSet<>();
+            Set<Integer> c = new HashSet<>();
+
+            System.out.print("How many students for course A? ");
+            int n = sc.nextInt();
+            for (int i=0; i<n; i++) {
+                int number = sc.nextInt();
+                a.add(number);
+            }
+
+            System.out.print("How many students for course B? ");
+            n = sc.nextInt();
+            for (int i=0; i<n; i++) {
+                int number = sc.nextInt();
+                b.add(number);
+            }
+
+            System.out.print("How many students for course C? ");
+            n = sc.nextInt();
+            for (int i=0; i<n; i++) {
+                int number = sc.nextInt();
+                c.add(number);
+            }
+
+            Set<Integer> total = new HashSet<>(a);
+            total.addAll(b);
+            total.addAll(c);
+
+            System.out.println("Total students: " + total.size());
+
+            sc.close();
 
 
-        sc.close();
     }
 }
