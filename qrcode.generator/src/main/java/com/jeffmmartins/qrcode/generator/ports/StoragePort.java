c4a9a1904bd4d67394b8b13ace516a783f87e71b
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-09-08 20:50:24 -0300

    Feat: adicionando comentários no projeto

--- qrcode.generator/src/main/java/com/jeffmmartins/qrcode/generator/ports/StoragePort.java
+++ qrcode.generator/src/main/java/com/jeffmmartins/qrcode/generator/ports/StoragePort.java
@@ -1,6 +1,6 @@
 package com.jeffmmartins.qrcode.generator.ports;
 
-
+//Definindo contrato das interfaces que precisa respeitar
 public interface StoragePort {
     String uploadFile(byte[] fileData, String fileName, String contentType);
 
