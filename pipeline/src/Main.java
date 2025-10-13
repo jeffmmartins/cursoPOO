05446e749469dd2eb08b69ede83dab138eee81ba
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-13 18:57:46 -0300

    feat: adicionando comentários no exemplo

--- pipeline/src/Main.java
+++ pipeline/src/Main.java
@@ -10,12 +10,16 @@ public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(3,4,5,10,7);
 
+
+        //Aplicando o stream junto com o map, dentro do map foi utilizado a lambda
         Stream<Integer> st1 = list.stream().map(x -> x*10);
         System.out.println(Arrays.toString(st1.toArray()));
 
+        //Soma o acumuldor + o valor atual.
         int sum = list.stream().reduce(0,(x,y) -> x+y);
         System.out.println("Soma: " + sum);
 
+        //convertendo no final para list.
         List<Integer> newList = list.stream()
                 .filter(x -> x%2 == 0)
                 .map(x -> x*10)
