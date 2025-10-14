d57fba42cb06b552b9873fa5082b171942cdab08
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-14 18:04:56 -0300

    Feat: código para ler arquivo criado

--- exercicios_stream/src/Main.java
+++ exercicios_stream/src/Main.java
@@ -1,7 +1,37 @@
+import entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Full file path");
+        String path = sc.nextLine();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))) {
+            List<Product> list = new ArrayList<>();
+
+            //lendo a linha no arquivo
+            String line = br.readLine();
+            while (line != null){
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                //ler a proxima linha
+                line = br.readLine();
+            }
+        } catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+        }
+        sc.close();
     }
 }
