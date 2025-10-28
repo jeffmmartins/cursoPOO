90b43baf62b20bd040488d9b3767be1574bddc7f
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-28 20:15:08 -0300

    Feat: metodo findAll na classe UserResource criado

--- coursedevsuperior/src/main/java/com/github/jeffmmartins/course/resourses/UserResource.java
+++ coursedevsuperior/src/main/java/com/github/jeffmmartins/course/resourses/UserResource.java
@@ -1,4 +1,19 @@
 package com.github.jeffmmartins.course.resourses;
 
+import com.github.jeffmmartins.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.RecursiveTask;
+
+@RestController
+@RequestMapping("{/users}")
 public class UserResource {
+
+    //Response entity retorna respostas de requisições web
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L,"Maria", "maria@gmail", "997933189", "123454");
+        return ResponseEntity.ok().body(u);
+    }
 }
